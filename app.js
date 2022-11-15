67b021ce10ad7e1209a4dbe156f4ea45201bf266
Sam <samuel.waters@gmail.com>
2022-11-15 11:30:40 -0700

    first

---
+++ app.js
@@ -0,0 +1,52 @@
+const validator = require('validator')
+const chalk = require('chalk')
+const yargs = require('yargs')
+
+const log = console.log;
+
+// log(validator.isEmail('@example.com'))
+// log(chalk.green.strikethrough('Sucess'));
+
+//version control
+// yargs.version('1.1.0');
+
+//add command
+yargs.command({
+    command: 'add',
+    describe: 'Add a new note',
+    handler: function() {
+        log('Adding a new note');
+    }
+});
+
+//remove command
+yargs.command({
+    command:'remove',
+    describe: 'Remove a note',
+    handler: function() {
+        log('Removing a note');
+    }
+});
+
+//list command
+yargs.command({
+    command:'list',
+    describe: 'List all notes',
+    handler: function() {
+        log('Listing all notes');
+    }
+});
+
+//read command
+yargs.command({
+    command:'read',
+    describe: 'Read a note',
+    handler: function() {
+        log('Reading a note');
+    }
+});
+
+
+// add, read, remove, list
+
+log(yargs.argv);
